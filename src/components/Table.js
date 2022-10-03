47c0120ff23c7dde40b1c695091c9d8055e7a498
Gabriel Duarte <gabrielduarte1405@gmail.com>
2022-10-03 18:50:07 -0300

    Requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,61 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
+  valueCalc = (ask) => {
+    const resultado = Number(ask);
+    return Number(resultado).toFixed(2);
+  };
+
+  valueCalc2 = (value, ask) => {
+    const resultado = Number(ask) * Number(value);
+    return Number(resultado).toFixed(2);
+  };
+
   render() {
+    const { expenses } = this.props;
     return (
       <table>
-        <th>Descrição</th>
-        <th>Tag</th>
-        <th>Método de pagamento</th>
-        <th>Valor</th>
-        <th>Moeda</th>
-        <th>Câmbio utilizado</th>
-        <th>Valor convertido</th>
-        <th>Moeda de conversão</th>
-        <th>Editar/Excluir</th>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+
+        <tbody>
+          {
+            expenses.map((e, index) => (
+              <tr key={ index }>
+                <td>{e.description}</td>
+                <td>{e.tag}</td>
+                <td>{e.method}</td>
+                <td>{Number(e.value).toFixed(2)}</td>
+                <td>{e.exchangeRates[e.currency].name}</td>
+                <td>{this.valueCalc(e.exchangeRates[e.currency].ask)}</td>
+                <td>{this.valueCalc2(e.value, e.exchangeRates[e.currency].ask)}</td>
+                <td>Real</td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = ({ wallet }) => ({
+  expenses: wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.instanceOf(Array),
+}.isRequired;
+
+export default connect(mapStateToProps)(Table);
